28daa39fd4b133b8b8cf3d892567b855d5cd5b63
Vito Hsu <shuoyan.hsu@gmail.com>
2025-07-02 12:40:51 +0800

    modify

---
+++ cabin-a.php
@@ -0,0 +1,434 @@
+<?php
+  // 設定新的網址
+  header("HTTP/1.1 301 Moved Permanently");
+  header("Location: https://guo-mama.com/cabin-detail.html");
+  exit();
+
+  $mytitle ='築夢館小木屋 - 雲林華山郭媽媽小木屋 - 古坑小木屋';
+  $mydesc ='築夢館小木屋';
+  include 'header.php' 
+?>
+<section class="nav-space bg-page py-6 bg-primary" style="background: url(img/bg01_bottom01.png) no-repeat bottom, url(img/bg02_bottom.png) no-repeat top, url(img/bg.png) repeat;">
+    <div class="container">
+      <div class="d-flex text-center align-items-center">
+        <div class="w-100">
+          <h2 class="text-white font-weight-light">
+            <span class="mr-2"><img src="img/title_left_pic_01.svg" width="50" alt=""></span>築夢館小木屋<span class="ml-2"><img src="img/title_right_pic_01.svg" width="50" alt=""></span>
+          </h2>
+          <p class="text-white font-weight-light lead">CABIN</p>
+          <nav aria-label="breadcrumb" class="d-flex justify-content-center banner-breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.php">首頁</a></li>
+              <li class="breadcrumb-item active" aria-current="page">古坑華山郭媽媽</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+</section>
+<section class="pb-3 pt-6" style="
+  background-image: url(img/bg_01.png);
+  background-repeat: repeat;
+">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-7 mb-4 mb-md-0">
+            <div id="sync1" class="owl-carousel owl-theme">
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-03.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-04.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room02-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room02-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-03.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-04.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/garden-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/garden-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/kitchen-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/kitchen-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/outdoor-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/outdoor-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/quad-room01-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/quad-room01-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/quad-room01-03.jpg" alt="">
+              </div>
+            </div>
+            <div id="sync2" class="owl-carousel owl-theme">
+            <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-03.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room01-04.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room02-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/double-room02-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-03.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/livingroom-04.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/garden-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/garden-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/kitchen-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/kitchen-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/outdoor-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/outdoor-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/quad-room01-01.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/quad-room01-02.jpg" alt="">
+              </div>
+              <div class="item">
+                <img class="img-fluid" src="img/cabin-a/quad-room01-03.jpg" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5 text-style">
+            <h2>築夢館小木屋</h2>
+            <hr>
+            <P>郭媽媽小木屋<br>
+              座落在古坑鄉華山村，這裡是個香杉木建造而成的私人木屋社區，當您卸下工作壓力，放鬆心靈，晚上慵懶的躺在柔軟的大床上，讓大自然洗淨您的疲憊，早晨起來喝杯咖啡，享受山林間的蟲鳴鳥叫，忘卻所有煩憂，陪你度過一個輕輕鬆鬆的假期，讓您留下更多的回憶即美好心情，走出社區漫步悠閒的尋找小天梯＆情人橋，迎接美好的明天，為自己安排一趟森林粉多精之旅吧！<br>
+              想來走走的好朋友們記得提前與我聯絡喔</P>
+            <hr>
+            <p style="margin-bottom:0px;">1. 一般平日包棟住宿優惠方案</p>
+            <p style="margin-left:30px">平日為星期日～星期四<br>平日：2人～10人 入住9折優惠 1人約1500</p>
+            <!-- <p style="margin-bottom:0px;">2. 持國旅卡消費：包棟95折優惠含早餐及精緻下午茶／市價約16800</p> -->
+            <p>2. 學生專案：包棟95折優惠含早餐及精緻下午茶／市價約16800</p>
+            <p class="badge-warning mb-1 mb-3 font-weight-light p-3">
+              <strong style="font-size: 15px;">每棟限制僅停 2 部車</strong>
+            </p>
+            <!--   <p class="badge-warning mb-3 font-weight-light p-3">
+                <strong style="font-size: 15px;">攜帶寵物最多 2 隻</strong>
+              </p>
+            <p>提供美味早餐 / 素食早餐住宿，供應精緻中式或西式早餐，自助鮮奶、咖啡，豆漿。</p>
+            <p>木屋內有廚房皆可使用，可自備火鍋食材或代購，<span class="text-danger">禁止快炒及油炸，如有造成屋內油煙或廚具使用不當，造成污染需加收500元清潔費。</span></p>
+            <h3>小木屋特色</h3>
+            <ul>
+              <li>備有停車空間</li>
+              <li>卡拉ok歡唱，開放時間至22:00</li>
+              <li>提供烤肉場地（需加收500元場地清潔費）<br>
+                <p class="badge badge-warning mb-1 font-weight-light p-2">
+                  <strong>提供烤肉代訂服務<a href="http://bit.ly/2H4BxWz" target="_blank"> 查看烤肉食材</a></strong>
+                </p>
+              </li>
+              <li>提供嬰兒澡盆</li>
+              <li>兒童遊戲區</li>
+              <li>客運站接送（古坑華山站）</li>
+            </ul> -->
+            <div class="alert alert-warning">
+              <strong> <a href="tel:+886920962888" target="_blank">0920-962-888</a></strong><br>
+              <strong>LINE ID：962888</strong>
+            </div>
+          </div>
+      </div><!-- col-md-12 -->
+    </div><!-- row -->
+  </div><!-- container -->
+</section>
+<!-- <section class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 table-responsive">
+        <h4 class="mb-3">房價介紹</h4>
+        <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <td>適用時間</td>
+              <td>平日</td>
+              <td>假日（週五、週六、連續假日寒暑假及特殊假日）</td>
+              <td>農曆春節</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>房價（NTD）</td>
+              <td>6000</td>
+              <td>8000</td>
+              <td>12000</td>
+            </tr>
+            <tr>
+              <td>加床（NTD）最多3人</td>
+              <td>500</td>
+              <td>500</td>
+              <td>500</td>
+            </tr>
+          </tbody>
+        </table>
+        <p class="badge badge-warning p-2">包棟6人含活力早餐6份，加1張床含1份活力早餐</p>
+      </div>
+    </div>
+    <div class="row mt-3 justify-content-center">
+      <div class="col-xs-12 text-center">
+        <a href="notice.php" class="btn btn-primary btn-gradient my-2">住宿須知</a>
+      </div>
+    </div>
+  </div>
+</section> -->
+<section class="py-6" style="
+  background-image: url(img/bg_footprint.png), url(img/bg_fox.png), url(img/bg_01.png);
+  background-repeat: no-repeat ,no-repeat, repeat;
+  background-position: left 20px top 15px, right -270px bottom -220px;
+  background-size: 100px, 570px;
+">
+  <div class="container">
+    <h2 class="text-center">小木屋照片</h2>
+    <div class="row mb-4">
+      <div class="col-md-12 pic-list">
+        <h6>小木屋</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="小木屋" href="img/cabin-a/garden-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/garden-01.jpg" alt="小木屋外面">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="小木屋" href="img/cabin-a/garden-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/garden-02.jpg" alt="小木屋外面">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="小木屋" href="img/cabin-a/outdoor-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/outdoor-01.jpg" alt="小木屋外面">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="小木屋" href="img/cabin-a/outdoor-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/outdoor-02.jpg" alt="小木屋外面">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-md-12 pic-list">
+        <h6>客廳</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="客廳" href="img/cabin-a/livingroom-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/livingroom-01.jpg" alt="客廳">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="客廳" href="img/cabin-a/livingroom-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/livingroom-02.jpg" alt="客廳">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="客廳" href="img/cabin-a/livingroom-03.jpg">
+              <img class="img-fluid" src="img/cabin-a/livingroom-03.jpg" alt="客廳">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="客廳" href="img/cabin-a/livingroom-04.jpg">
+              <img class="img-fluid" src="img/cabin-a/livingroom-04.jpg" alt="客廳">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-md-12 pic-list">
+        <h6>廚房</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/kitchen-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/kitchen-01.jpg" alt="廚房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/kitchen-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/kitchen-02.jpg" alt="廚房">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="row mb-4">
+      <div class="col-md-12 pic-list">
+        <h6>早餐</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="早餐" href="img/cabin-a/breakfast-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/breakfast-01.jpg" alt="早餐">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="早餐" href="img/cabin-a/breakfast-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/breakfast-02.jpg" alt="早餐">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="早餐" href="img/cabin-a/breakfast-03.jpg">
+              <img class="img-fluid" src="img/cabin-a/breakfast-03.jpg" alt="早餐">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="早餐" href="img/cabin-a/breakfast-04.jpg">
+              <img class="img-fluid" src="img/cabin-a/breakfast-04.jpg" alt="早餐">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <div class="row mb-4">
+      <div class="col-md-12 pic-list">
+        <h6>4人房間</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/double-room01-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/double-room01-01.jpg" alt="4人房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/double-room01-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/double-room01-02.jpg" alt="4人房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/double-room01-03.jpg">
+              <img class="img-fluid" src="img/cabin-a/double-room01-03.jpg" alt="4人房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/double-room01-04.jpg">
+              <img class="img-fluid" src="img/cabin-a/double-room01-04.jpg" alt="4人房">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-md-12 pic-list">
+        <h6>4人房間</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/quad-room01-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/quad-room01-01.jpg" alt="4人房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/quad-room01-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/quad-room01-02.jpg" alt="4人房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/quad-room01-03.jpg">
+              <img class="img-fluid" src="img/cabin-a/quad-room01-03.jpg" alt="4人房">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 pic-list">
+        <h6>2人房間</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-0 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="2人房" href="img/cabin-a/double-room02-01.jpg">
+              <img class="img-fluid" src="img/cabin-a/double-room02-01.jpg" alt="2人房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-0 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="2人房" href="img/cabin-a/double-room02-02.jpg">
+              <img class="img-fluid" src="img/cabin-a/double-room02-02.jpg" alt="2人房">
+            </a>
+          </div>
+          <div class="col-6 col-md-3 mb-0 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="2人房" href="img/cabin-a/double-room02-03.jpg">
+              <img class="img-fluid" src="img/cabin-a/double-room02-03.jpg" alt="2人房">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="row">
+      <div class="col-md-12 pic-list">
+        <h6>沙發床</h6>
+        <div class="row">
+          <div class="col-6 col-md-3 mb-0 mb-md-0">
+            <a data-fancybox="cabin-a" data-caption="4人房" href="img/cabin-a/persons-04.jpg">
+              <img class="img-fluid" src="img/cabin-a/persons-04.jpg" alt="沙發床">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-5 justify-content-center">
+      <div class="col-xs-12 text-center">
+        <a href="notice.php" class="btn btn-primary btn-gradient my-2">住宿須知</a>
+      </div>
+    </div> -->
+  </div>
+</section>
+<?php include 'footer.php' ?>
+<?php include 'script.php' ?>
